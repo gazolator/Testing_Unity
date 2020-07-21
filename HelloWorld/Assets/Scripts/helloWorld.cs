1a127ce9f81642792e50dd04f9fca863c2505660
fish <fishcompanylp@gmail.com>
2020-07-21 21:12:03 +0100

    testing console
    testing console

--- HelloWorld/Assets/Scripts/helloWorld.cs
+++ HelloWorld/Assets/Scripts/helloWorld.cs
@@ -7,7 +7,7 @@ public class helloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log("Thanos"); 
     }
 
     // Update is called once per frame
